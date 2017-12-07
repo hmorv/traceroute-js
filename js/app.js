0d7232e6c6f075dafd56e530025530ed4e28c882
Hugo Moragues <hugo.moragues@gmail.com>
2017-12-07 10:27:23 +0100

    t3

--- js/app.js
+++ js/app.js
@@ -2,8 +2,25 @@
 	'use strict';
 
 	var traceroute = require('traceroute');
+
+	var results = {};
+
 	traceroute.trace('google.com', function(err, hops) {
 		if(!err)
-			console.log(hops);
+			//console.log(hops);
+		results.google = hops.length;
+		traceroute.trace('bing.com', function(err, hops) {
+		if(!err)
+			//console.log(hops);
+			results.bing = hops.length;
+			traceroute.trace('duckduckgo.com', function(err, hops) {
+				if(!err)
+					//console.log(hops);
+				results.duckduckgo = hops.length;
+			});
+		});
 	});
+	
+
+	console.log(results);
 })();
